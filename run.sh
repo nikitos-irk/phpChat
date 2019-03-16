a33b09c284e8b1906fd2b8e82dd5906796aca93a
Nikita Kovrigin <Nikita.Kovrigin@dell.com>
2019-03-16 23:28:28 +0300

    bash script to create bd and some data inside

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+dbname=t.db
+sqlite3 $dbname " 
+CREATE TABLE IF NOT EXISTS users (
+    user_id integer PRIMARY KEY,
+    user_name VARCHAR(256)
+);"
+
+sqlite3 $dbname "
+CREATE TABLE messages (
+	message_id INTEGER PRIMARY KEY,
+	time_stamp DATETIME,
+	message VARCHAR(4000),
+	read_status BOOLEAN,
+	sender_id integer NOT NULL,
+		FOREIGN KEY (sender_id) REFERENCES users(user_id)
+);"
+
+sqlite3 $dbname "
+CREATE TABLE message_recipients (
+	message_id integer NOT NULL,
+	recipient_id integer NOT NULL,
+		FOREIGN KEY (message_id) REFERENCES messages(message_id),
+		FOREIGN KEY (recipient_id) REFERENCES users(user_id)
+);"
+
+sqlite3 $dbname "
+insert into users (user_name) values (\"nk\");
+insert into users (user_name) values (\"rk\");
+insert into messages (time_stamp, message, read_status, sender_id) values (datetime('now'), \"hello from nk\", 0, 1);
+insert into messages (time_stamp, message, read_status, sender_id) values (datetime('now'), \"hello from rk\", 0, 2);
+insert into message_recipients (message_id, recipient_id) values (1, 2);
+insert into message_recipients (message_id, recipient_id) values (2, 1);
+"
